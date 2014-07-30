e7e8964a6ca6efdfeab2035ee63c8c19decb1b28
haivv.itedu <haivv.itedu@gmail.com>
2014-07-30 14:11:21 +0000

    [Change]

--- src/java/converter/RoleIDConverter.java
+++ src/java/converter/RoleIDConverter.java
@@ -1,5 +1,7 @@
 package converter;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -8,36 +10,36 @@ import model.dao.RoleDAOImpl;
 import model.dao.service.RoleDAO;
 import model.entities.Role;
 
-@FacesConverter("roleConverter")
+@ManagedBean
+@RequestScoped
+@FacesConverter("roleIDConverter")
 public class RoleIDConverter implements Converter {
 
     private final RoleDAO ROLE_SERVICE = RoleDAOImpl.getInstance();
 
-    /**
-     * Creates a new instance of RoleIDConverter
-     */
-    public RoleIDConverter() {
-    }
-
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        Role roleName = null;
+        Role role = null;
         if (value != null) {
-            roleName = ROLE_SERVICE.getRoleByName(value);
-            roleName.setRoleName(value);
+            role = ROLE_SERVICE.getRoleByName(value);
+            role.setRoleName(value);
         }
-        return roleName;
+        System.out.println("get: " + role.getRoleName() + " - " + role.getRoleId());
+        return role;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        String roleName = "";
+        String name = "";
         if (value instanceof Role) {
             Role role = (Role) value;
-            roleName = role.getRoleName();
+            name = role.getRoleName();
+            System.out.println("if: " + name);
         } else if (value instanceof String) {
-            roleName = (String) value;
+            name = (String) value;
+            System.out.println("else: " + name);
         }
-        return roleName;
+        return name;
     }
+
 }
